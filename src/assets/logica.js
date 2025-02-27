8b30ab6658a13ba8777a3f532c14f819b6a07605
brrodriguezd <brrodriguezd@unal.edu.co>
2025-02-27 10:14:51 -0500

    Integracion

--- src/assets/logica.js
+++ src/assets/logica.js
@@ -1,33 +1,56 @@
-import router from '@/main'
+import router from "@/main";
+import axios from "axios";
 
 function cambiarTitulo(titulo) {
-	document.getElementById("tituloGeneral").innerHTML = titulo
+  document.getElementById("tituloGeneral").innerHTML = titulo;
 }
 
-function ingreso() {
-	let usuario = document.getElementById("InputEmail1").value
-	let contraseña = document.getElementById("InputPassword1").value
+async function ingreso() {
+  let usuario = document.getElementById("InputEmail1").value;
+  let contraseña = document.getElementById("InputPassword1").value;
 
-	console.log("Usuario: " + usuario)
-	console.log("Contraseña: " + contraseña)
-	//Logica de back
-
-	router.push("/")
+  try {
+    const response = await axios.post("http://localhost:5000/auth/login", {
+      email: usuario,
+      password: contraseña,
+    });
+    console.log("Logged in:");
+    router.push("/");
+  } catch (error) {
+    console.error("Login error:", error);
+  }
+  //Logica de back
 }
 
-async function buscarECBE() {
-	return {
-		"id": "uuid_del_fragmento",
-		"content": "Texto coincidente",
-		"document_id": "uuid_del_documento",
-		"filename": "archivo.pdf",
-		"similarity": 0.85
-	}
+async function buscarECBE(query) {
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:5000/search",
+      {
+        query: query,
+        threshold: 0.7, // optional
+        limit: 5, // optional
+      },
+      {
+        headers: {
+          Authorization: "Bearer token",
+        },
+      },
+    );
+    let results = response.data["results"][0];
+    return {
+      id: results["id"],
+      content: results["content"],
+      filename: results["filename"],
+      similarity: results["similarity"],
+    };
+  } catch (error) {
+    console.error("Search error:", error);
+  }
 }
-
 
 export default {
-	cambiarTitulo,
-	ingreso,
-	buscarECBE
-}
+  cambiarTitulo,
+  ingreso,
+  buscarECBE,
+};
